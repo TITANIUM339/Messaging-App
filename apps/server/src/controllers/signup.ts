923146085d490f65bab92e0d1b2a254ae7a8f90e
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-10-10 16:53:41 +0300

    Create signup controller

---
+++ apps/server/src/controllers/signup.ts
@@ -0,0 +1,53 @@
+import bcrypt from "bcrypt";
+import { eq } from "drizzle-orm";
+import type { Request, Response } from "express";
+import * as z from "zod";
+import db from "../db";
+import { users } from "../db/schema";
+
+const User = z.object({
+    username: z
+        .string()
+        .trim()
+        .nonempty({ abort: true })
+        .max(32, { abort: true })
+        .refine(
+            async (data) => {
+                try {
+                    const [user] = await db
+                        .select()
+                        .from(users)
+                        .where(eq(users.username, data));
+
+                    if (user) {
+                        return false;
+                    }
+                } catch {
+                    return false;
+                }
+
+                return true;
+            },
+            { error: "Username already exists" },
+        ),
+    password: z.string().nonempty(),
+});
+
+export default {
+    async post(req: Request, res: Response) {
+        const result = await User.safeParseAsync(req.body);
+
+        if (!result.success) {
+            res.status(400).json(result.error.issues);
+
+            return;
+        }
+
+        await db.insert(users).values({
+            username: result.data.username,
+            passwordHash: await bcrypt.hash(result.data.password, 10),
+        });
+
+        res.sendStatus(201);
+    },
+};
