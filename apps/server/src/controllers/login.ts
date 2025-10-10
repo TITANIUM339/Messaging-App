885fff61366056eaac2144cf261da8603d741907
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-10-10 16:59:50 +0300

    Create login controller

---
+++ apps/server/src/controllers/login.ts
@@ -0,0 +1,37 @@
+import { REFRESH_TOKEN_COOKIE } from "@lib/constants";
+import jwt from "@lib/jwt";
+import type { Request, Response } from "express";
+import db from "../db";
+import { refreshTokens, users } from "../db/schema";
+
+export default {
+    async post(req: Request, res: Response) {
+        const user = req.user as typeof users.$inferSelect;
+
+        const [refreshToken, accessToken] = await Promise.all([
+            jwt.sing({ id: user.id }, process.env.REFRESH_TOKEN_SECRET!, {
+                expiresIn: "7d",
+            }),
+            jwt.sing({ id: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
+                expiresIn: "10m",
+            }),
+        ]);
+
+        await db
+            .insert(refreshTokens)
+            .values({
+                token: refreshToken,
+                expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+                userId: user.id,
+            })
+            .onConflictDoNothing();
+
+        res.cookie(
+            REFRESH_TOKEN_COOKIE.name,
+            refreshToken,
+            REFRESH_TOKEN_COOKIE.options,
+        );
+
+        res.json({ accessToken });
+    },
+};
