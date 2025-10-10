33c0c49880910925c1bea01c2d6837d25b8cc99f
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-10-10 17:04:33 +0300

    Create refreshToken controller

---
+++ apps/server/src/controllers/refreshToken.ts
@@ -0,0 +1,37 @@
+import { REFRESH_TOKEN_COOKIE } from "@lib/constants";
+import jwt from "@lib/jwt";
+import { eq } from "drizzle-orm";
+import type { Request, Response } from "express";
+import db from "../db";
+import { refreshTokens, users } from "../db/schema";
+
+export default {
+    async get(req: Request, res: Response) {
+        const user = req.user as typeof users.$inferSelect;
+
+        const [refreshToken, accessToken] = await Promise.all([
+            jwt.sing({ id: user.id }, process.env.REFRESH_TOKEN_SECRET!, {
+                expiresIn: "7d",
+            }),
+            jwt.sing({ id: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
+                expiresIn: "10m",
+            }),
+        ]);
+
+        await db
+            .update(refreshTokens)
+            .set({
+                token: refreshToken,
+                expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            })
+            .where(eq(refreshTokens.token, req.cookies.refreshToken));
+
+        res.cookie(
+            REFRESH_TOKEN_COOKIE.name,
+            refreshToken,
+            REFRESH_TOKEN_COOKIE.options,
+        );
+
+        res.json({ accessToken });
+    },
+};
