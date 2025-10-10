88e21dd553a83031974e472a81a4b6c112195599
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-10-10 17:06:59 +0300

    Export all routes

---
+++ apps/server/src/routes/index.ts
@@ -0,0 +1,6 @@
+import login from "./login";
+import logout from "./logout";
+import refreshToken from "./refresh-token";
+import signup from "./signup";
+
+export default [login, logout, refreshToken, signup];
