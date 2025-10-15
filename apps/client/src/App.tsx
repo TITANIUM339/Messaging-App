11d7a47bf0d8bbcff0db6da9064c745bce2c9d08
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-10-15 21:20:00 +0300

    Create signup route

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 export default function App() {
     const router = createBrowserRouter([
         {
             path: "/login",
             Component: Login,
+        },
+        {
+            path: "/signup",
+            Component: Signup,
         },
     ]);
 
