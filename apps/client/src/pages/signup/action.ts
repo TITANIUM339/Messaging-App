1b437ca58398f9053600a5d55a4871dd4dea549e
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-10-22 16:50:31 +0300

    Signup user

---
+++ apps/client/src/pages/signup/action.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/only-throw-error */
+import { redirect, type ActionFunctionArgs } from "react-router";
+import * as z from "zod";
+import api from "../../lib/api";
+import { Login } from "../../lib/schema";
+
+export default async function action({ request }: ActionFunctionArgs) {
+    const formData = await request.formData();
+
+    const result = Login.safeParse({
+        username: formData.get("username"),
+        password: formData.get("password"),
+    });
+
+    if (!result.success) {
+        return z.treeifyError(result.error);
+    }
+
+    const response = await api.fetch(
+        "/signup",
+        {
+            method: "post",
+            body: JSON.stringify(result.data),
+        },
+        false,
+    );
+
+    if (response.status === 400) {
+        return (await response.json()) as unknown;
+    }
+
+    if (!response.ok) {
+        throw response;
+    }
+
+    await api.login(result.data.username, result.data.password);
+
+    return redirect("/");
+}
