77d38dfb58126eabc8a80789341a996d77ac1267
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-10-23 16:42:26 +0300

    Use layout

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,19 +1,25 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Auth from "./layouts/Auth";
 import Login, { loginAction } from "./pages/login";
 import Signup, { signupAction } from "./pages/signup";
 
 export default function App() {
     const router = createBrowserRouter([
         {
-            path: "/login",
-            Component: Login,
-            action: loginAction,
-        },
-        {
-            path: "/signup",
-            Component: Signup,
-            action: signupAction,
+            Component: Auth,
+            children: [
+                {
+                    path: "/login",
+                    Component: Login,
+                    action: loginAction,
+                },
+                {
+                    path: "/signup",
+                    Component: Signup,
+                    action: signupAction,
+                },
+            ],
         },
     ]);
 
--- apps/client/src/pages/signup/Signup.tsx
+++ apps/client/src/pages/signup/Signup.tsx
@@ -17,90 +17,70 @@ export default function Signup() {
     const navigation = useNavigation();
 
     return (
-        <div className="grid min-h-dvh grid-rows-[min-content_1fr] bg-zinc-900 p-2 text-zinc-100">
-            <header>
-                <div className="p-4">
-                    <Link to="/" className="text-2xl font-bold">
-                        Messaging App
-                    </Link>
+        <section className="w-full max-w-2xl rounded-lg bg-zinc-800 p-8 shadow-lg">
+            <h1 className="text-center text-2xl font-medium">
+                Create an account
+            </h1>
+            <Form className="mt-4 flex flex-col gap-6" method="post">
+                <div className="flex flex-col gap-2">
+                    <label className="font-medium text-zinc-200" htmlFor={id1}>
+                        Username <span className="text-red-300">*</span>
+                    </label>
+                    <input
+                        className="rounded-lg border border-zinc-700 bg-zinc-900 p-2 text-zinc-200 outline-0 transition-colors user-invalid:border-red-300 focus:border-sky-600"
+                        type="text"
+                        name="username"
+                        autoComplete="username"
+                        autoFocus
+                        id={id1}
+                        maxLength={32}
+                        spellCheck={false}
+                        required
+                    />
+                    {data?.properties?.username ? (
+                        <div className="flex items-center gap-1 text-sm text-red-300">
+                            <BsExclamationCircleFill />
+                            <p>{data.properties.username.errors[0]}</p>
+                        </div>
+                    ) : null}
                 </div>
-            </header>
-            <main className="flex items-center justify-center">
-                <section className="w-full max-w-2xl rounded-lg bg-zinc-800 p-8 shadow-lg">
-                    <h1 className="text-center text-2xl font-medium">
-                        Create an account
-                    </h1>
-                    <Form className="mt-4 flex flex-col gap-6" method="post">
-                        <div className="flex flex-col gap-2">
-                            <label
-                                className="font-medium text-zinc-200"
-                                htmlFor={id1}
-                            >
-                                Username <span className="text-red-300">*</span>
-                            </label>
-                            <input
-                                className="rounded-lg border border-zinc-700 bg-zinc-900 p-2 text-zinc-200 outline-0 transition-colors user-invalid:border-red-300 focus:border-sky-600"
-                                type="text"
-                                name="username"
-                                autoComplete="username"
-                                autoFocus
-                                id={id1}
-                                maxLength={32}
-                                spellCheck={false}
-                                required
-                            />
-                            {data?.properties?.username ? (
-                                <div className="flex items-center gap-1 text-sm text-red-300">
-                                    <BsExclamationCircleFill />
-                                    <p>{data.properties.username.errors[0]}</p>
-                                </div>
-                            ) : null}
+                <div className="flex flex-col gap-2">
+                    <label className="font-medium text-zinc-200" htmlFor={id2}>
+                        Password <span className="text-red-300">*</span>
+                    </label>
+                    <input
+                        className="rounded-lg border border-zinc-700 bg-zinc-900 p-2 text-zinc-200 outline-0 transition-colors user-invalid:border-red-300 focus:border-sky-600"
+                        type="password"
+                        name="password"
+                        autoComplete="new-password"
+                        id={id2}
+                        required
+                    />
+                    {data?.properties?.password ? (
+                        <div className="flex items-center gap-1 text-sm text-red-300">
+                            <BsExclamationCircleFill />
+                            <p>{data.properties.password.errors[0]}</p>
                         </div>
-                        <div className="flex flex-col gap-2">
-                            <label
-                                className="font-medium text-zinc-200"
-                                htmlFor={id2}
-                            >
-                                Password <span className="text-red-300">*</span>
-                            </label>
-                            <input
-                                className="rounded-lg border border-zinc-700 bg-zinc-900 p-2 text-zinc-200 outline-0 transition-colors user-invalid:border-red-300 focus:border-sky-600"
-                                type="password"
-                                name="password"
-                                autoComplete="new-password"
-                                id={id2}
-                                required
-                            />
-                            {data?.properties?.password ? (
-                                <div className="flex items-center gap-1 text-sm text-red-300">
-                                    <BsExclamationCircleFill />
-                                    <p>{data.properties.password.errors[0]}</p>
-                                </div>
-                            ) : null}
-                        </div>
-                        <Button
-                            className="flex justify-center"
-                            type="submit"
-                            disabled={navigation.state !== "idle"}
-                        >
-                            {navigation.state !== "idle" ? (
-                                <Spinner size={24} />
-                            ) : (
-                                "Sign Up"
-                            )}
-                        </Button>
-                    </Form>
-                    <p className="mt-2 text-zinc-400">
-                        Already have an account?{" "}
-                        <Link
-                            className="text-indigo-300 hover:underline"
-                            to="/login"
-                        >
-                            Log In
-                        </Link>
-                    </p>
-                </section>
-            </main>
-        </div>
+                    ) : null}
+                </div>
+                <Button
+                    className="flex justify-center"
+                    type="submit"
+                    disabled={navigation.state !== "idle"}
+                >
+                    {navigation.state !== "idle" ? (
+                        <Spinner size={24} />
+                    ) : (
+                        "Sign Up"
+                    )}
+                </Button>
+            </Form>
+            <p className="mt-2 text-zinc-400">
+                Already have an account?{" "}
+                <Link className="text-indigo-300 hover:underline" to="/login">
+                    Log In
+                </Link>
+            </p>
+        </section>
     );
 }
