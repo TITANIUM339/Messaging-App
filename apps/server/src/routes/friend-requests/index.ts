bf99bef2ef3b06e35f21f53a42fe1802bdaa77c1
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-10-28 22:50:18 +0300

    Add router

--- apps/server/src/routes/friend-requests/index.ts
+++ apps/server/src/routes/friend-requests/index.ts
@@ -1,15 +1,15 @@
 import addFriend from "@controllers/addFriend";
 import { Router } from "express";
 import passport from "passport";
+import friendRequestRouter from "./friend-request";
 
 const router = Router();
 
 const route = "/friend-requests";
 
-router.post(
-    route,
-    passport.authenticate("accessToken", { session: false }),
-    addFriend.post,
-);
+router.use(route, passport.authenticate("accessToken", { session: false }));
+router.use(route, friendRequestRouter);
+
+router.post(route, addFriend.post);
 
 export default router;
