60f8e7fb270324c4fc41c24b4b6fc18e1e063e2a
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-11-04 13:29:10 +0300

    Add routes

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,11 +1,31 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Auth from "./layouts/Auth";
+import Add from "./pages/add";
+import Friends from "./pages/friends";
 import Login, { loginAction } from "./pages/login";
+import Root, { rootLoader } from "./pages/root";
 import Signup, { signupAction } from "./pages/signup";
 
 export default function App() {
     const router = createBrowserRouter([
+        {
+            path: "/",
+            loader: rootLoader,
+            Component: Root,
+            children: [
+                {
+                    path: "friends",
+                    Component: Friends,
+                    children: [
+                        {
+                            path: "add",
+                            Component: Add,
+                        },
+                    ],
+                },
+            ],
+        },
         {
             Component: Auth,
             children: [
