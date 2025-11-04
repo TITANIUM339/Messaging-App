a3f652e7689f24fbd86787657c15842d29c96756
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-11-04 13:26:20 +0300

    Create root page

---
+++ apps/client/src/pages/root/Root.tsx
@@ -0,0 +1,83 @@
+import { BsFillPeopleFill, BsPlusLg } from "react-icons/bs";
+import { Outlet } from "react-router";
+import { twMerge } from "tailwind-merge";
+import Button from "../../components/Button";
+
+export default function Root() {
+    return (
+        <div className="grid min-h-dvh grid-cols-[200px_1fr] text-zinc-100">
+            <nav className="bg-zinc-900 p-2">
+                <ul>
+                    <li>
+                        <Button
+                            className={({ isActive }) => {
+                                const baseStyles =
+                                    "flex w-full items-center gap-2";
+
+                                if (isActive) {
+                                    return twMerge(baseStyles, "bg-zinc-600");
+                                }
+
+                                return baseStyles;
+                            }}
+                            navLink
+                            to="/friends"
+                            variant="secondary"
+                        >
+                            <BsFillPeopleFill /> Friends
+                        </Button>
+                    </li>
+                </ul>
+                <hr className="mt-2 text-zinc-700" />
+                <div className="mt-2">
+                    <section className="flex items-center justify-between p-2">
+                        <h1 className="text-zinc-400">Conversations</h1>
+                        <Button variant="secondary">
+                            <BsPlusLg />
+                        </Button>
+                    </section>
+                    <ul className="flex flex-col gap-1">
+                        {new Array(9).fill(null).map((_, i) => (
+                            <li key={i}>
+                                <Button
+                                    className={({ isActive }) => {
+                                        const baseStyles =
+                                            "flex w-full items-center gap-2";
+
+                                        if (isActive) {
+                                            return twMerge(
+                                                baseStyles,
+                                                "bg-zinc-600",
+                                            );
+                                        }
+
+                                        return baseStyles;
+                                    }}
+                                    navLink
+                                    to="/conversations/1"
+                                    variant="secondary"
+                                >
+                                    <div className="h-8 w-8 overflow-hidden rounded-full">
+                                        <img
+                                            className="object-cover"
+                                            src="https://picsum.photos/200/300"
+                                            alt=""
+                                        />
+                                    </div>
+                                    <section className="flex-1 truncate">
+                                        <h2 className="font-medium text-zinc-400">
+                                            Friend
+                                        </h2>
+                                    </section>
+                                </Button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </nav>
+            <main className="bg-zinc-800">
+                <Outlet />
+            </main>
+        </div>
+    );
+}
