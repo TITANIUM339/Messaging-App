77bdf48dd1c92ae5ef8aeb1c51204e1ffc8e9aeb
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-11-05 20:06:48 +0300

    Send friend request

---
+++ apps/client/src/pages/add/action.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/only-throw-error */
+import type { ActionFunctionArgs } from "react-router";
+import { treeifyError } from "zod";
+import api from "../../lib/api";
+import { Username } from "../../lib/schema";
+
+export default async function action({ request }: ActionFunctionArgs) {
+    const formData = await request.formData();
+
+    const result = Username.safeParse({ username: formData.get("username") });
+
+    if (!result.success) {
+        return { success: false, error: treeifyError(result.error) };
+    }
+
+    const response = await api.fetch("/friend-requests", {
+        method: "post",
+        body: JSON.stringify(result.data),
+    });
+
+    if (response.status === 400) {
+        return { success: false, error: (await response.json()) as unknown };
+    }
+
+    if (!response.ok) {
+        throw response;
+    }
+
+    return { success: true };
+}
