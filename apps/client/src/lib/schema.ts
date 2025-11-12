3c138899b54bf2418058cbe730ac760b168e4344
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-11-12 15:29:34 +0300

    Create zod schemas

--- apps/client/src/lib/schema.ts
+++ apps/client/src/lib/schema.ts
@@ -10,3 +10,21 @@ export const AccessToken = z.object({
 });
 
 export const Username = z.object({ username: z.string().nonempty().max(32) });
+
+export const FriendRequests = z.array(
+    z.object({
+        senderId: z.int(),
+        senderUsername: z.string(),
+        receiverId: z.int(),
+        receiverUsername: z.string(),
+    }),
+);
+
+export const User = z.object({
+    id: z.int(),
+});
+
+export const FriendRequestId = z.object({
+    senderId: z.coerce.number().pipe(z.int()),
+    receiverId: z.coerce.number().pipe(z.int()),
+});
