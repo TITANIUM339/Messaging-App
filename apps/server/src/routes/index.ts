07b33b41618eab741ad9ae9c6ebc5e24a75b615b
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-11-16 17:29:03 +0300

    Add friends router

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,7 +1,8 @@
 import friendRequests from "./friend-requests";
+import friends from "./friends";
 import login from "./login";
 import logout from "./logout";
 import refreshToken from "./refresh-token";
 import signup from "./signup";
 
-export default [friendRequests, login, logout, refreshToken, signup];
+export default [friendRequests, friends, login, logout, refreshToken, signup];
