61b953bd978d2e19e94cf6101aca049f9f39117e
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-11-17 13:50:41 +0300

    Throw unsuccessful responses

--- apps/client/src/pages/pending/loader.ts
+++ apps/client/src/pages/pending/loader.ts
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/only-throw-error */
 import api from "@lib/api";
 import { FriendRequests } from "@lib/schema";
 
 export default async function loader() {
     const response = await api.fetch("/friend-requests");
+
+    if (!response.ok) {
+        throw response;
+    }
 
     return FriendRequests.parse(await response.json());
 }
