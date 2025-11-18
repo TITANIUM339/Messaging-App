2b491d1fd719040f5da616bddf4856e107bae704
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-11-18 17:44:38 +0300

    Add friend router

--- apps/server/src/routes/friends/index.ts
+++ apps/server/src/routes/friends/index.ts
@@ -1,12 +1,14 @@
 import friends from "@controllers/friends";
 import { Router } from "express";
 import passport from "passport";
+import friendRouter from "./friend";
 
 const router = Router();
 
 const route = "/friends";
 
 router.use(route, passport.authenticate("accessToken", { session: false }));
+router.use(route, friendRouter);
 
 router.get(route, friends.get);
 
