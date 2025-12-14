06b59833575dda901bd9b11450bc1b9d94ce91dd
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-12-14 20:13:21 +0300

    Verify jwt

--- apps/server/src/lib/jwt.ts
+++ apps/server/src/lib/jwt.ts
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export default {
     sing(
         payload: string | object | Buffer<ArrayBufferLike>,
-        secret: jwt.Secret | jwt.PrivateKey,
+        secret: jwt.Secret,
         options: jwt.SignOptions,
     ): Promise<string> {
         return new Promise((resolve, reject) => {
@@ -15,6 +15,19 @@ export default {
                 }
 
                 resolve(token);
+            });
+        });
+    },
+    verify(token: string, secret: jwt.Secret, options?: jwt.VerifyOptions) {
+        return new Promise((resolve, reject) => {
+            jwt.verify(token, secret, options, (error, decoded) => {
+                if (error || !decoded) {
+                    reject(error ? error : new Error("Unknown error"));
+
+                    return;
+                }
+
+                resolve(decoded);
             });
         });
     },
