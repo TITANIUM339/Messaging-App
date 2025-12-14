508e593f16b3ddcae92809b991fddec3aa8b919c
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-12-14 20:15:18 +0300

    Authenticate new socket connections

---
+++ apps/server/src/middleware/authenticateSocket.ts
@@ -0,0 +1,52 @@
+import jwt from "@lib/jwt";
+import { eq } from "drizzle-orm";
+import jsonwebtoken from "jsonwebtoken";
+import type { Socket } from "socket.io";
+import * as z from "zod";
+import db from "../db";
+import { users } from "../db/schema";
+
+const Token = z.jwt();
+
+const UserId = z.object({
+    id: z.int().min(1),
+});
+
+export default async function authenticateSocket(
+    socket: Socket,
+    next: (error?: Error) => void,
+) {
+    try {
+        const token = Token.parse(socket.handshake.auth.token);
+
+        const decoded = UserId.parse(
+            await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!),
+        );
+
+        const [user] = await db
+            .select({ id: users.id, username: users.username })
+            .from(users)
+            .where(eq(users.id, decoded.id));
+
+        socket.data.user = user;
+    } catch (error) {
+        if (
+            error instanceof z.ZodError ||
+            error instanceof jsonwebtoken.TokenExpiredError ||
+            error instanceof jsonwebtoken.JsonWebTokenError ||
+            error instanceof jsonwebtoken.NotBeforeError
+        ) {
+            next(new Error("Unauthorized"));
+
+            return;
+        }
+
+        console.error(error);
+
+        next(new Error("Internal Server Error"));
+
+        return;
+    }
+
+    next();
+}
