2abdd0107f67c420ed0c25af9d5d96a199848240
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2026-01-20 11:39:07 +0300

    Add route

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -2,7 +2,16 @@ import friendRequests from "./friend-requests";
 import friends from "./friends";
 import login from "./login";
 import logout from "./logout";
+import pgp from "./pgp";
 import refreshToken from "./refresh-token";
 import signup from "./signup";
 
-export default [friendRequests, friends, login, logout, refreshToken, signup];
+export default [
+    friendRequests,
+    friends,
+    login,
+    logout,
+    pgp,
+    refreshToken,
+    signup,
+];
