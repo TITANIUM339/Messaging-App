86f2b0e41080fe0d5594f6e0584d436c3bea8864
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2026-01-20 11:29:21 +0300

    After generating a new PGP key pair prompt the user to save their private key

--- apps/client/src/pages/signup/Signup.tsx
+++ apps/client/src/pages/signup/Signup.tsx
@@ -1,9 +1,10 @@
 import Button from "@components/Button";
+import Dialog from "@components/Dialog";
 import Spinner from "@components/Spinner";
 import { Login } from "@lib/schema";
-import { useId } from "react";
-import { BsExclamationCircleFill } from "react-icons/bs";
-import { Form, Link, useActionData, useNavigation } from "react-router";
+import { useEffect, useId, useRef, useState } from "react";
+import { BsCopy, BsDownload, BsExclamationCircleFill } from "react-icons/bs";
+import { Link, useFetcher } from "react-router";
 import * as z from "zod";
 
 type TreeifiedError = ReturnType<typeof z.treeifyError<z.infer<typeof Login>>>;
@@ -12,16 +13,46 @@ export default function Signup() {
     const id1 = useId(),
         id2 = useId();
 
-    const data = useActionData<TreeifiedError>();
+    const fetcher = useFetcher();
 
-    const navigation = useNavigation();
+    const dialogRef = useRef<HTMLDialogElement | null>(null);
+
+    const [downloadUrl, setDownloadUrl] = useState("");
+
+    const data = fetcher.data as
+        | { success: true; privateKey: string }
+        | { success: false; error: TreeifiedError }
+        | undefined;
+
+    const privateKey = data?.success ? data.privateKey : null;
+    const error = !data?.success ? data?.error : null;
+
+    if (privateKey) {
+        dialogRef.current?.showModal();
+    }
+
+    useEffect(() => {
+        if (!privateKey) {
+            return;
+        }
+
+        const objectUrl = URL.createObjectURL(
+            new Blob([privateKey], {
+                type: "text/plain",
+            }),
+        );
+
+        setDownloadUrl(objectUrl);
+
+        return () => URL.revokeObjectURL(objectUrl);
+    }, [privateKey]);
 
     return (
         <section className="w-full max-w-2xl rounded-lg bg-zinc-800 p-8 shadow-lg">
             <h1 className="text-center text-2xl font-medium">
                 Create an account
             </h1>
-            <Form className="mt-4 flex flex-col gap-6" method="post">
+            <fetcher.Form className="mt-4 flex flex-col gap-6" method="post">
                 <div className="flex flex-col gap-2">
                     <label className="font-medium text-zinc-200" htmlFor={id1}>
                         Username <span className="text-red-300">*</span>
@@ -37,10 +68,10 @@ export default function Signup() {
                         spellCheck={false}
                         required
                     />
-                    {data?.properties?.username ? (
+                    {error?.properties?.username ? (
                         <div className="flex items-center gap-1 text-sm text-red-300">
                             <BsExclamationCircleFill />
-                            <p>{data.properties.username.errors[0]}</p>
+                            <p>{error.properties.username.errors[0]}</p>
                         </div>
                     ) : null}
                 </div>
@@ -56,31 +87,76 @@ export default function Signup() {
                         id={id2}
                         required
                     />
-                    {data?.properties?.password ? (
+                    {error?.properties?.password ? (
                         <div className="flex items-center gap-1 text-sm text-red-300">
                             <BsExclamationCircleFill />
-                            <p>{data.properties.password.errors[0]}</p>
+                            <p>{error.properties.password.errors[0]}</p>
                         </div>
                     ) : null}
                 </div>
                 <Button
                     className="flex justify-center"
                     type="submit"
-                    disabled={navigation.state !== "idle"}
+                    disabled={fetcher.state !== "idle"}
                 >
-                    {navigation.state !== "idle" ? (
+                    {fetcher.state !== "idle" ? (
                         <Spinner size={24} />
                     ) : (
                         "Sign Up"
                     )}
                 </Button>
-            </Form>
+            </fetcher.Form>
             <p className="mt-2 text-zinc-400">
                 Already have an account?{" "}
                 <Link className="text-indigo-300 hover:underline" to="/login">
                     Log In
                 </Link>
             </p>
+            <Dialog ref={dialogRef} closedby="none">
+                <h1 className="text-xl font-bold">
+                    Store your private key somewhere safe
+                </h1>
+                <p className="mt-1 text-zinc-400">
+                    This key is used for end-to-end encryption. Make sure you
+                    save it because you <em>will</em> need it to log in later.
+                    Do <em>not</em> share this key with anyone!
+                </p>
+                <pre className="mt-4 rounded-lg bg-zinc-900 p-2 wrap-break-word whitespace-pre-wrap text-zinc-300">
+                    {privateKey}
+                </pre>
+                <div className="mt-4 flex flex-wrap gap-2">
+                    <Button
+                        className="flex flex-1 items-center justify-center gap-2"
+                        onClick={() => {
+                            if (privateKey) {
+                                navigator.clipboard
+                                    .writeText(privateKey)
+                                    .catch((error) => console.error(error));
+                            }
+                        }}
+                        autoFocus
+                    >
+                        <BsCopy /> Copy
+                    </Button>
+                    <Button
+                        className="flex flex-1 items-center justify-center gap-2"
+                        link
+                        to={downloadUrl}
+                        download="private_key"
+                        reloadDocument
+                    >
+                        <BsDownload /> Download
+                    </Button>
+                    <Button
+                        className="flex-1 text-center"
+                        variant="warning"
+                        link
+                        to="/"
+                    >
+                        Continue
+                    </Button>
+                </div>
+            </Dialog>
         </section>
     );
 }
