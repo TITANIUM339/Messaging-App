2a7584bbd363332ee86d887fa3ccaa3f3e793616
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2026-01-20 11:13:28 +0300

    Log out user if private key in local storage is invalid

--- apps/client/src/pages/root/loader.ts
+++ apps/client/src/pages/root/loader.ts
@@ -1,8 +1,42 @@
+/* eslint-disable @typescript-eslint/only-throw-error */
 import api from "@lib/api";
+import { decryptKey, readKey, readPrivateKey } from "openpgp";
 import { redirect } from "react-router";
 
-export default function loader() {
+async function logout() {
+    const errorResponse = await api.logout();
+
+    if (errorResponse) {
+        throw errorResponse;
+    }
+
+    return redirect("/login");
+}
+
+export default async function loader() {
     if (!api.isLoggedIn) {
         return redirect("/login");
+    }
+
+    try {
+        const key = localStorage.getItem(`key-${api.user?.id}`);
+
+        if (!key) {
+            return await logout();
+        }
+
+        const privateKey = await readPrivateKey({ armoredKey: key });
+        const publicKey = await readKey({ armoredKey: api.user!.publicKey });
+
+        if (!privateKey.getKeyID().equals(publicKey.getKeyID())) {
+            return await logout();
+        }
+
+        return await decryptKey({
+            privateKey,
+            passphrase: api.user?.passphrase,
+        });
+    } catch {
+        return await logout();
     }
 }
