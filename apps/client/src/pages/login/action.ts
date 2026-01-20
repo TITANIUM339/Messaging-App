09db29eb4c935da2dc81ed0f035a0570accc3226
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2026-01-20 11:44:07 +0300

    Check for valid private key

--- apps/client/src/pages/login/action.ts
+++ apps/client/src/pages/login/action.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/only-throw-error */
 import api from "@lib/api";
 import { Login } from "@lib/schema";
+import { readKey, readPrivateKey } from "openpgp";
 import { redirect, type ActionFunctionArgs } from "react-router";
 
 export default async function action({ request }: ActionFunctionArgs) {
@@ -12,7 +13,7 @@ export default async function action({ request }: ActionFunctionArgs) {
     });
 
     if (!result.success) {
-        return result.error;
+        return "invalid_form";
     }
 
     const response = await api.login(
@@ -20,12 +21,29 @@ export default async function action({ request }: ActionFunctionArgs) {
         result.data.password,
     );
 
-    if (response?.status === 401 || response?.status == 400) {
-        return response;
+    if (response?.status === 401 || response?.status === 400) {
+        return "invalid_form";
     }
 
     if (response) {
         throw response;
+    }
+
+    try {
+        const key = localStorage.getItem(`key-${api.user?.id}`);
+
+        if (!key) {
+            return "no_key";
+        }
+
+        const privateKey = await readPrivateKey({ armoredKey: key });
+        const publicKey = await readKey({ armoredKey: api.user!.publicKey });
+
+        if (!privateKey.getKeyID().equals(publicKey.getKeyID())) {
+            return "no_key";
+        }
+    } catch {
+        return "no_key";
     }
 
     return redirect("/");
