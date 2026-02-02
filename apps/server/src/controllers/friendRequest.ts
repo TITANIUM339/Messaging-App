423113128bff66dd088b1b1cea345c3cb82a88cb
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2026-02-02 18:05:54 +0300

    Fix issue where friend would appear offline after accepting their friend request

--- apps/server/src/controllers/friendRequest.ts
+++ apps/server/src/controllers/friendRequest.ts
@@ -5,38 +5,56 @@ import { friendRequests, friends } from "../db/schema";
 
 export default {
     async post(req: Request, res: Response) {
+        const io = req.app.io;
+
         const senderId = +req.params.senderId!;
         const receiverId = +req.params.receiverId!;
 
-        await db.transaction(async (tx) => {
-            await Promise.all([
-                tx
-                    .delete(friendRequests)
-                    .where(
-                        and(
-                            or(
-                                eq(friendRequests.senderId, senderId),
-                                eq(friendRequests.senderId, receiverId),
-                            ),
-                            or(
-                                eq(friendRequests.receiverId, receiverId),
-                                eq(friendRequests.receiverId, senderId),
+        const [senderSockets, receiverSockets] = await Promise.all([
+            io.to(`${senderId}`).fetchSockets(),
+            io.to(`${receiverId}`).fetchSockets(),
+            db.transaction(async (tx) => {
+                await Promise.all([
+                    tx
+                        .delete(friendRequests)
+                        .where(
+                            and(
+                                or(
+                                    eq(friendRequests.senderId, senderId),
+                                    eq(friendRequests.senderId, receiverId),
+                                ),
+                                or(
+                                    eq(friendRequests.receiverId, receiverId),
+                                    eq(friendRequests.receiverId, senderId),
+                                ),
                             ),
                         ),
-                    ),
 
-                tx.insert(friends).values([
-                    {
-                        userId: senderId,
-                        friendOf: receiverId,
-                    },
-                    {
-                        userId: receiverId,
-                        friendOf: senderId,
-                    },
-                ]),
-            ]);
-        });
+                    tx.insert(friends).values([
+                        {
+                            userId: senderId,
+                            friendOf: receiverId,
+                        },
+                        {
+                            userId: receiverId,
+                            friendOf: senderId,
+                        },
+                    ]),
+                ]);
+            }),
+        ]);
+
+        if (senderSockets.length && receiverSockets.length) {
+            io.to(`${senderId}`).emit(
+                "friend_connected",
+                receiverSockets[0]?.data.user,
+            );
+
+            io.to(`${receiverId}`).emit(
+                "friend_connected",
+                senderSockets[0]?.data.user,
+            );
+        }
 
         res.sendStatus(201);
     },
