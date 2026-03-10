9186a71bad619894b9e200ddeea277a73c554943
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2026-03-10 14:25:16 +0300

    Add chat router

--- apps/server/src/routes/chats/index.ts
+++ apps/server/src/routes/chats/index.ts
@@ -1,12 +1,14 @@
 import chats from "@controllers/chats";
 import { Router } from "express";
 import passport from "passport";
+import chatRouter from "./chat";
 
 const router = Router();
 
 const route = "/chats";
 
 router.use(route, passport.authenticate("accessToken", { session: false }));
+router.use(route, chatRouter);
 
 router.route(route).get(chats.get);
 
