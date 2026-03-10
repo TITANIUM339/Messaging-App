a451ac62b79113211a271e5f560d5d3a575c9bc3
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2026-03-10 14:29:40 +0300

    Add members router

--- apps/server/src/routes/chats/chat/index.ts
+++ apps/server/src/routes/chats/chat/index.ts
@@ -1,12 +1,15 @@
 import chat from "@controllers/chat";
 import checkChat from "@middleware/checkChat";
 import { Router } from "express";
+import membersRouter from "./members";
 
 const router = Router();
 
 const route = "/:chatId";
 
 router.param("chatId", checkChat);
+
+router.use(route, membersRouter);
 
 router.get(route, chat.get);
 
