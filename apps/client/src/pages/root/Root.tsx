577e1dc41779de90a80ea1f5a52e1b9a42eae477
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2026-03-11 13:50:05 +0300

    Render chats

--- apps/client/src/pages/root/Root.tsx
+++ apps/client/src/pages/root/Root.tsx
@@ -4,12 +4,22 @@ import api from "@lib/api";
 import type { Friends } from "@lib/schema";
 import { useEffect, useRef, useState } from "react";
 import { BsFillPeopleFill, BsPlusLg } from "react-icons/bs";
-import { Outlet, useNavigate } from "react-router";
+import { Outlet, useLoaderData, useNavigate } from "react-router";
 import { twMerge } from "tailwind-merge";
 import type * as z from "zod";
 
 export default function Root() {
     const navigate = useNavigate();
+
+    const data = useLoaderData<{
+        privateChats: { id: number; userId: number; username: string }[];
+        groupChats: {
+            id: number;
+            title: string;
+            icon?: string;
+            owner: number;
+        }[];
+    }>();
 
     const timeoutRef = useRef<number | null>(null);
 
@@ -86,8 +96,8 @@ export default function Root() {
 
     return (
         <div className="grid min-h-dvh grid-cols-[200px_1fr] text-zinc-100">
-            <nav className="bg-zinc-900 p-2">
-                <ul>
+            <nav className="grid grid-rows-[60px_1fr] bg-zinc-900 p-2">
+                <ul className="border-b border-zinc-700 pb-2">
                     <li>
                         <Button
                             className={({ isActive }) => {
@@ -108,51 +118,113 @@ export default function Root() {
                         </Button>
                     </li>
                 </ul>
-                <hr className="mt-2 text-zinc-700" />
                 <div className="mt-2">
-                    <section className="flex items-center justify-between p-2">
-                        <h1 className="text-zinc-400">Conversations</h1>
-                        <Button variant="secondary">
-                            <BsPlusLg />
-                        </Button>
+                    <h1 className="text-lg font-medium text-zinc-300">Chats</h1>
+                    <section className="p-2">
+                        <h2 className="text-zinc-400">Private</h2>
+                        <ul className="mt-1 flex flex-col gap-1">
+                            {data.privateChats.map((chat) => (
+                                <li key={chat.id}>
+                                    <Button
+                                        className={({ isActive }) => {
+                                            const baseStyles =
+                                                "group flex w-full items-center gap-2";
+
+                                            if (isActive) {
+                                                return twMerge(
+                                                    baseStyles,
+                                                    "bg-zinc-600",
+                                                );
+                                            }
+
+                                            return baseStyles;
+                                        }}
+                                        navLink
+                                        to={`/chats/${chat.id}`}
+                                        variant="secondary"
+                                    >
+                                        {({ isActive }) => (
+                                            <>
+                                                <div className="relative">
+                                                    <div className="relative h-8 w-8 overflow-hidden rounded-full">
+                                                        <img
+                                                            className="object-cover"
+                                                            src={`https://api.dicebear.com/9.x/identicon/svg?seed=${chat.username}`}
+                                                            alt=""
+                                                        />
+                                                    </div>
+                                                    <div
+                                                        className={twMerge(
+                                                            "absolute right-0 bottom-0 size-4 translate-[3px] rounded-full border-3 border-zinc-900 bg-zinc-500 group-hover:border-zinc-700",
+                                                            isActive &&
+                                                                "border-zinc-600",
+                                                            connectedFriends.some(
+                                                                (
+                                                                    connectedFriend,
+                                                                ) =>
+                                                                    connectedFriend.id ===
+                                                                    chat.userId,
+                                                            ) && "bg-green-500",
+                                                        )}
+                                                    ></div>
+                                                </div>
+                                                <section className="flex-1 truncate">
+                                                    <h2 className="font-medium text-zinc-400">
+                                                        {chat.username}
+                                                    </h2>
+                                                </section>
+                                            </>
+                                        )}
+                                    </Button>
+                                </li>
+                            ))}
+                        </ul>
                     </section>
-                    <ul className="flex flex-col gap-1">
-                        {new Array(9).fill(null).map((_, i) => (
-                            <li key={i}>
-                                <Button
-                                    className={({ isActive }) => {
-                                        const baseStyles =
-                                            "flex w-full items-center gap-2";
+                    <section className="p-2">
+                        <div className="flex items-center justify-between">
+                            <h2 className="text-zinc-400">Group</h2>
+                            <Button variant="secondary">
+                                <BsPlusLg />
+                            </Button>
+                        </div>
+                        <ul className="mt-1 flex flex-col gap-1">
+                            {data.groupChats.map((chat) => (
+                                <li key={chat.id}>
+                                    <Button
+                                        className={({ isActive }) => {
+                                            const baseStyles =
+                                                "flex w-full items-center gap-2";
 
-                                        if (isActive) {
-                                            return twMerge(
-                                                baseStyles,
-                                                "bg-zinc-600",
-                                            );
-                                        }
+                                            if (isActive) {
+                                                return twMerge(
+                                                    baseStyles,
+                                                    "bg-zinc-600",
+                                                );
+                                            }
 
-                                        return baseStyles;
-                                    }}
-                                    navLink
-                                    to="/conversations/1"
-                                    variant="secondary"
-                                >
-                                    <div className="h-8 w-8 overflow-hidden rounded-full">
-                                        <img
-                                            className="object-cover"
-                                            src="https://picsum.photos/200/300"
-                                            alt=""
-                                        />
-                                    </div>
-                                    <section className="flex-1 truncate">
-                                        <h2 className="font-medium text-zinc-400">
-                                            Friend
-                                        </h2>
-                                    </section>
-                                </Button>
-                            </li>
-                        ))}
-                    </ul>
+                                            return baseStyles;
+                                        }}
+                                        navLink
+                                        to={`/chats/${chat.id}`}
+                                        variant="secondary"
+                                    >
+                                        <div className="h-8 w-8 overflow-hidden rounded-full">
+                                            <img
+                                                className="object-cover"
+                                                src={`https://api.dicebear.com/9.x/identicon/svg?seed=${chat.title}`}
+                                                alt="Avatar"
+                                            />
+                                        </div>
+                                        <section className="flex-1 truncate">
+                                            <h2 className="font-medium text-zinc-400">
+                                                {chat.title}
+                                            </h2>
+                                        </section>
+                                    </Button>
+                                </li>
+                            ))}
+                        </ul>
+                    </section>
                 </div>
             </nav>
             <main className="bg-zinc-800">
