a77503f4e374469834beb5f9b3282bb58a12004d
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2026-03-14 14:42:36 +0300

    Encrypt messages

--- apps/client/src/pages/chat/action.ts
+++ apps/client/src/pages/chat/action.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/only-throw-error */
 import api from "@lib/api";
 import { Message } from "@lib/schema";
+import { createMessage, encrypt, readKey } from "openpgp";
 import type { ActionFunctionArgs } from "react-router";
 
 export default async function action({ request, params }: ActionFunctionArgs) {
@@ -8,13 +9,13 @@ export default async function action({ request, params }: ActionFunctionArgs) {
 
     const message = Message.parse(formData.get("message"));
 
-    const response = await api.fetch(`/chats/${params.chatId}/members`);
+    const response1 = await api.fetch(`/chats/${params.chatId}/members`);
 
-    if (!response.ok) {
-        throw response;
+    if (!response1.ok) {
+        throw response1;
     }
 
-    const members = (await response.json()) as {
+    const members = (await response1.json()) as {
         id: number;
         username: string;
         publicKey: string;
@@ -23,11 +24,21 @@ export default async function action({ request, params }: ActionFunctionArgs) {
     const response2 = await api.fetch(`/chats/${params.chatId}/messages`, {
         method: request.method,
         body: JSON.stringify(
-            members.map((member) => ({ to: member.id, content: message })),
+            await Promise.all(
+                members.map(async (member) => ({
+                    to: member.id,
+                    content: (await encrypt({
+                        message: await createMessage({ text: message }),
+                        encryptionKeys: await readKey({
+                            armoredKey: member.publicKey,
+                        }),
+                    })) as string,
+                })),
+            ),
         ),
     });
 
     if (!response2.ok) {
-        throw response;
+        throw response2;
     }
 }
